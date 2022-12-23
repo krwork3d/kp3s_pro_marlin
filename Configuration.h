8038a263252ff8aca47ab593d08ca2fd4106576c
krwork3d <121272911+krwork3d@users.noreply.github.com>
2022-12-23 06:19:39 +0300

    update

--- Configuration.h
+++ Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(kubik369)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(krwork3d)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
